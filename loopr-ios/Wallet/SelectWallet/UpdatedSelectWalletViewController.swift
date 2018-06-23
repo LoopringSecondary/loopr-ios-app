5c68c528010d8304c9cf60f17646ff773429207e
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-06-23 00:11:14 -0700

    Able to switch wallet.

--- loopr-ios/Wallet/SelectWallet/UpdatedSelectWalletViewController.swift
+++ loopr-ios/Wallet/SelectWallet/UpdatedSelectWalletViewController.swift
@@ -25,8 +25,17 @@ class UpdatedSelectWalletViewController: UIViewController, UITableViewDelegate, 
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
         self.navigationItem.title = NSLocalizedString("Wallet", comment: "")
+        
+        walletTableView.delegate = self
+        walletTableView.dataSource = self
+        walletTableView.estimatedRowHeight = 2.0
+        walletTableView.separatorStyle = .none
+        walletTableView.rowHeight = UITableViewAutomaticDimension
+        walletTableView.tableFooterView = UIView()
+        
+        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: 200, height: 4))
+        walletTableView.tableHeaderView = headerView
         
         createButton.setTitle(NSLocalizedString("Generate Wallet", comment: ""), for: .normal)
         createButton.setupRoundPurple(height: 48, font: UIFont.init(name: FontConfigManager.shared.getRegular(), size: 16))
@@ -35,7 +44,11 @@ class UpdatedSelectWalletViewController: UIViewController, UITableViewDelegate, 
         importButton.setTitle(NSLocalizedString("Import Wallet", comment: ""), for: .normal)
         importButton.setupRoundPurpleOutline(height: 48, font: UIFont.init(name: FontConfigManager.shared.getRegular(), size: 16))
         importButton.addTarget(self, action: #selector(pressedImportButton(_:)), for: UIControlEvents.touchUpInside)
-
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        walletTableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,6 +64,7 @@ class UpdatedSelectWalletViewController: UIViewController, UITableViewDelegate, 
         viewController.modalPresentationStyle = .overFullScreen
 
         let newNavigationController = UINavigationController()
+        newNavigationController.modalPresentationStyle = .overFullScreen
         newNavigationController.setViewControllers([viewController], animated: false)
         self.navigationController?.present(newNavigationController, animated: true, completion: {
             
@@ -66,12 +80,16 @@ class UpdatedSelectWalletViewController: UIViewController, UITableViewDelegate, 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return AppWalletDataManager.shared.getWallets().count
     }
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UpdatedSelectWalletTableViewCell.getHeight()
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: SelectWalletTableViewCell.getCellIdentifier()) as? SelectWalletTableViewCell
+        var cell = tableView.dequeueReusableCell(withIdentifier: UpdatedSelectWalletTableViewCell.getCellIdentifier()) as? UpdatedSelectWalletTableViewCell
         if cell == nil {
-            let nib = Bundle.main.loadNibNamed("SelectWalletTableViewCell", owner: self, options: nil)
-            cell = nib![0] as? SelectWalletTableViewCell
+            let nib = Bundle.main.loadNibNamed("UpdatedSelectWalletTableViewCell", owner: self, options: nil)
+            cell = nib![0] as? UpdatedSelectWalletTableViewCell
         }
         
         // Configure the cell...
@@ -79,4 +97,23 @@ class UpdatedSelectWalletViewController: UIViewController, UITableViewDelegate, 
         cell?.update()
         return cell!
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let appWallet = AppWalletDataManager.shared.getWallets()[indexPath.row]
+        if appWallet.address != CurrentAppWalletDataManager.shared.getCurrentAppWallet()!.address {
+            let alertController = UIAlertController(title: NSLocalizedString("Choose Wallet", comment: "") + ": \(appWallet.name)",
+                message: nil,
+                preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: NSLocalizedString("Confirm", comment: ""), style: .default, handler: { _ in
+                CurrentAppWalletDataManager.shared.setCurrentAppWallet(appWallet)
+                self.walletTableView.reloadData()
+            })
+            alertController.addAction(defaultAction)
+            let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel, handler: { _ in
+            })
+            alertController.addAction(cancelAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+
 }
