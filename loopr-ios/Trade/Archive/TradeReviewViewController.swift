416e682650b1d1347d5900d056cae94c9caf6730
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-06-26 19:00:23 -0700

    Remove the blur effect of a QR code.

--- loopr-ios/Trade/Archive/TradeReviewViewController.swift
+++ loopr-ios/Trade/Archive/TradeReviewViewController.swift
@@ -17,6 +17,11 @@ class TradeReviewViewController: UIViewController {
     var tokenBView: TradeTokenView!
     var arrowRightImageView: UIImageView = UIImageView()
     var qrcodeImageView: UIImageView!
+    
+    // To display QR code
+    var qrcodeImageCIImage: CIImage!
+    
+    // To share QR code
     var qrcodeImage: UIImage!
 
     var order: OriginalOrder?
@@ -80,8 +85,16 @@ class TradeReviewViewController: UIViewController {
         // self.navigationController?.isNavigationBarHidden = false
         tokenSView.update(title: "You send", symbol: order.tokenSell, amount: order.amountSell)
         tokenBView.update(title: "You get", symbol: order.tokenBuy, amount: order.amountBuy)
+        
         generateQRCode(order: order)
         qrcodeImageView.image = qrcodeImage
+        
+        // Remove the blur effect
+        let scaleX = qrcodeImageView.frame.size.width / qrcodeImageCIImage.extent.size.width
+        let scaleY = qrcodeImageView.frame.size.height / qrcodeImageCIImage.extent.size.height
+        
+        let transformedImage = qrcodeImageCIImage.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
+        qrcodeImageView.image = UIImage.init(ciImage: transformedImage)
     }
     
     func generateQRCode(order: OriginalOrder) {
@@ -94,6 +107,9 @@ class TradeReviewViewController: UIViewController {
             filter.setValue(data, forKey: "inputMessage")
             let transform = CGAffineTransform(scaleX: 2, y: 2)
             let upScaledImage = filter.outputImage?.transformed(by: transform)
+            
+            qrcodeImageCIImage = upScaledImage!
+            
             let cgImage = ciContext.createCGImage(upScaledImage!, from: upScaledImage!.extent)
             qrcodeImage = UIImage(cgImage: cgImage!)
         }
