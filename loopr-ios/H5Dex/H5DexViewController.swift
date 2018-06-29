b6a9dae14ff5fe60be5c096846c73162641af943
iwbrhwfy <kenshin_6@163.com>
2018-06-29 11:27:37 +0800

    modify h5dex

--- loopr-ios/H5Dex/H5DexViewController.swift
+++ loopr-ios/H5Dex/H5DexViewController.swift
@@ -11,10 +11,11 @@ import UIKit
 import WebKit
 import JavaScriptCore
 
-class H5DexViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler, UIScrollViewDelegate {
+class H5DexViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler, UIScrollViewDelegate, UITextFieldDelegate {
 
-    var webView: WKWebView!
-
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var urlTextField: UITextField!
+    
     var dexRequest: DexRequest!
     
     var start = Date()
@@ -25,44 +26,33 @@ class H5DexViewController: UIViewController, WKNavigationDelegate, WKScriptMessa
 
         // Do any additional setup after loading the view.
         self.navigationController?.isNavigationBarHidden = true
-        createWebView()
         H5DexDataManager.shared.sendClosure = self.sendDataToHtml
+        urlTextField.delegate = self
+        urlTextField.returnKeyType = UIReturnKeyType.done
+        webView.navigationDelegate = self
+        webView.scrollView.delegate = self
+        self.webView.translatesAutoresizingMaskIntoConstraints = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func createWebView() {
-        let contentController = WKUserContentController()
-        contentController.add(self, name: "nativeCallbackHandler")
-        let config = WKWebViewConfiguration()
-        config.userContentController = contentController
-
-        webView = WKWebView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height), configuration: config)
-        let url = URL(string: "http://10.137.109.34:8000/#/auth/loopr")
-        let request = URLRequest(url: url!)
-        webView.load(request)
-        
-        self.view.addSubview(webView)
-        
-        // Auto Layout
-        let topConst = NSLayoutConstraint(item: self.webView, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.top, multiplier: 1, constant: UIApplication.shared.statusBarFrame.height)
-        let botConst = NSLayoutConstraint(item: self.webView, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0)
-        let leftConst = NSLayoutConstraint(item: self.webView, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.leading, multiplier: 1, constant: 0)
-        let rigthConst = NSLayoutConstraint(item: self.webView, attribute: NSLayoutAttribute.trailing, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: 0)
-        
-        NSLayoutConstraint.activate([topConst, botConst, leftConst, rigthConst])
-        self.webView.translatesAutoresizingMaskIntoConstraints = false
-        
-        webView.navigationDelegate = self
-        webView.scrollView.delegate = self
-    }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        urlTextField.resignFirstResponder()
+        return false
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         start = Date()
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let url = URL(string: textField.text!)
+        let request = URLRequest(url: url!)
+        webView.load(request)
     }
     
     func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
@@ -87,20 +77,15 @@ class H5DexViewController: UIViewController, WKNavigationDelegate, WKScriptMessa
             if let objStr = message.body as? String {
                 let data: Data = objStr.data(using: .utf8)!
                 let json = JSON(data)
-                self.dexRequest = DexRequest(json: json) // dexRequest.data == JSON.null
+                self.dexRequest = DexRequest(json: json)
                 H5DexDataManager.shared.handle(request: dexRequest)
             }
         }
     }
 
     func sendDataToHtml(json: JSON) {
-        
-        let a = json.rawString(.utf8)!
-        print(a)
-        print(self.dexRequest.callback)
-        
         if let jsonString = json.rawString(.utf8), let callback = self.dexRequest.callback {
-            self.webView.evaluateJavaScript("\(callback)(\(jsonString))") { result, error in
+            self.webView.evaluateJavaScript("\(callback)(\(jsonString))") { _, error in
                 guard error == nil else {
                     print(error!)
                     return
